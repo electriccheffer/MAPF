8aed318b7627b26f50d900d80a0fe2625c87b346
electricchef <electriccheff@gmail.com>
2025-10-14 18:14:06 -0500

    TEST PASSED ObstaclePosition equality false case

--- test/test_position.cpp
+++ test/test_position.cpp
@@ -29,6 +29,19 @@ TEST(PositionTest,EqualityTestTrue){
 	EXPECT_EQ(position,otherPosition); 
 }
 
+TEST(PositionTest,EqualityTestFalse){
+	
+	float xCoordinate = 1.0;
+	float yCoordinate = 2.3; 
+	float otherXCoordinate = 1.3; 
+	float otherYCoordinate = -1.2; 
+	std::vector<float> expectedPositionVector = {xCoordinate,yCoordinate}; 
+	ObstaclePosition2D position(xCoordinate,yCoordinate);
+	ObstaclePosition2D otherPosition(otherXCoordinate,otherYCoordinate); 
+	EXPECT_FALSE((position==otherPosition));
+
+}
+
 TEST(PositionTest,NonEqualityTestNotEqual){	
 	
 	float xCoordinate = 1.0;
