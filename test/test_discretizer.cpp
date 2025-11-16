11c0dd4507f5816367c20d1c7a9693117e6c11f9
electricchef <electriccheff@gmail.com>
2025-11-16 09:35:34 -0600

    TEST PASSED ObstacleDiscretizer makeEdge

--- test/test_discretizer.cpp
+++ test/test_discretizer.cpp
@@ -77,7 +77,7 @@ std::vector<ObstaclePosition2D> descretePointsFactory(){
 
 }
 
-TEST(TestDiscritizer,makeEdge){
+TEST(TestDiscritizer,makeEdgeVertical){
 
 	float positionX = 0.0; 
 	float positionY = 0.0; 
@@ -93,15 +93,60 @@ TEST(TestDiscritizer,makeEdge){
 	ObstacleDimension2D unitDirection = {unitHeight, unitWidth}; 
 	ObstacleDiscretizer2DSubclass discretizer(obstacle,interval);
 	ObstaclePosition2D endBoundary(positionX,height); 
-	std::cout << position << std::endl; 
-	std::cout << unitDirection << std::endl; 
-	std::cout <<"END POINT" << endBoundary << std::endl; 
 	std::vector<ObstaclePosition2D> result = discretizer.makeEdgeTestAccess(position,unitDirection,endBoundary); 
 	std::vector<ObstaclePosition2D> expected = {ObstaclePosition2D(0.0,0.01),
 						   ObstaclePosition2D(0.0,0.02),ObstaclePosition2D(0.0,0.03),
 						   ObstaclePosition2D(0.0,0.04),ObstaclePosition2D(0.0,0.05),
 						   ObstaclePosition2D(0.0,0.06),ObstaclePosition2D(0.0,0.07),
 						   ObstaclePosition2D(0.0,0.08),ObstaclePosition2D(0.0,0.09)}; 
+	
+	EXPECT_EQ(expected,result); 
+}
+
+TEST(TestDiscritizer,makeEdgeHorizontal){
+
+	float positionX = 0.0; 
+	float positionY = 0.0; 
+	float height = .10; 
+	float width = .10;
+       	float space  = 0.1; 	
+	float unitHeight = 0.05; 
+	float unitWidth = 0.00; 
+	ObstaclePosition2D position(positionX,positionY); 
+	ObstacleDimension2D dimension(height,width);
+	Obstacle2D obstacle(position,dimension); 
+	DiscreteInterval2D interval(space,dimension); 
+	ObstacleDimension2D unitDirection = {unitHeight, unitWidth}; 
+	ObstacleDiscretizer2DSubclass discretizer(obstacle,interval);
+	ObstaclePosition2D endBoundary(height,0.0); 
+	std::vector<ObstaclePosition2D> result = discretizer.makeEdgeTestAccess(position,unitDirection,endBoundary); 
+	std::vector<ObstaclePosition2D> expected = {ObstaclePosition2D(.05,0.0)}; 
+	
+	EXPECT_EQ(expected,result); 
+}
+
+TEST(TestDiscritizer,makeEdgeHorizontalVertical){
+
+	float positionX = 0.1; 
+	float positionY = 0.0; 
+	float height = .10; 
+	float width = .10;
+       	float space  = .01; 	
+	float unitHeight = 0.01; 
+	float unitWidth = 0.0; 
+	ObstaclePosition2D position(positionX,positionY); 
+	ObstacleDimension2D dimension(height,width);
+	Obstacle2D obstacle(position,dimension); 
+	DiscreteInterval2D interval(space,dimension); 
+	ObstacleDimension2D unitDirection = {unitWidth,unitHeight}; 
+	ObstacleDiscretizer2DSubclass discretizer(obstacle,interval);
+	ObstaclePosition2D endBoundary(positionX,height); 
+	std::vector<ObstaclePosition2D> result = discretizer.makeEdgeTestAccess(position,unitDirection,endBoundary); 
+	std::vector<ObstaclePosition2D> expected = {ObstaclePosition2D(0.1,0.01),
+						   ObstaclePosition2D(0.1,0.02),ObstaclePosition2D(0.1,0.03),
+						   ObstaclePosition2D(0.1,0.04),ObstaclePosition2D(0.1,0.05),
+						   ObstaclePosition2D(0.1,0.06),ObstaclePosition2D(0.1,0.07),
+						   ObstaclePosition2D(0.1,0.08),ObstaclePosition2D(0.1,0.09)}; 
 	
 	EXPECT_EQ(expected,result); 
 }
