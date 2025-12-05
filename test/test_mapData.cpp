522404c8d2fcbaec46b76dcc13fa91db579681b0
electricchef <electriccheff@gmail.com>
2025-12-05 11:51:44 -0600

    TRIVIAL TEST FOR MapData

---
+++ test/test_mapData.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include <vector>
+#include "../include/Obstacles.hpp"
+#include "../include/errors/ObstacleErrors.hpp"
+#include <iostream>
+
+TEST(TestMapData,TrivialTestAlwaysPasses){
+
+	EXPECT_TRUE(true); 
+}
+
+TEST(TestMapData,TestGetObstacleByPositionTrue){
+
+	
+
+}
