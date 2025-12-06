72cd4d4ccfb80d0a456a7cf861b2d99a1f304968
electricchef <electriccheff@gmail.com>
2025-12-06 14:49:45 -0600

    TEST PASSED MapData for get false case

--- test/test_mapData.cpp
+++ test/test_mapData.cpp
@@ -90,3 +90,23 @@ TEST(TestMapData,RetrievePositionTrue){
 	EXPECT_TRUE(inMap); 
 
 }
+
+TEST(TestMapData,RetrievePositionFalse){
+
+	float xPosition = 2.4; 
+	float yPosition = 4.2; 
+	float spaceInterval = .1; 
+	float height = spaceInterval; 
+	float width = spaceInterval;	
+	ObstacleDimension2D dimension(height,width);
+	DiscreteInterval2D interval(spaceInterval,dimension); 	
+	ObstaclePosition2D position(xPosition,yPosition);
+        float otherX = 2.33; 
+     	float otherY = 2.3; 
+	ObstaclePosition2D otherPosition(otherX,otherY);	
+	MapData<ObstaclePosition2D,DiscreteInterval2D,MapTuple2D> mapData(interval); 
+	mapData.add(position);
+      	bool inMap = mapData.get(otherPosition);
+	EXPECT_FALSE(inMap); 
+
+}
